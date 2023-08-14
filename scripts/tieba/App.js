fdbb6c819cbcb5063ae4efac34c1fcfe3c925f54
cbdyzj <cbdyzj@jianzhao.org>
2023-08-14 23:31:18 +0800

    Amend

--- scripts/tieba/App.js
+++ scripts/tieba/App.js
@@ -1,10 +1,24 @@
-import { html } from './deps.js'
+import { css, html } from './deps.js'
 
-export default class App  extends preact.Component{
+const ClassName = css`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  font-weight: bold;
+
+  > span {
+    font-size: 5rem;
+    transform: translateY(-16vh);
+  }
+`
+
+export default class App extends preact.Component {
 
     render() {
         return html`
-            <div>贴吧</div>
+            <div class="${ClassName}">
+                <span>让贴吧再次伟大！</span>
+            </div>
         `
     }
 }
--- scripts/tieba/deps.js
+++ scripts/tieba/deps.js
@@ -1,5 +1,6 @@
 import 'https://alchemy-works.github.io/magic/preact_pack.js'
 import 'https://alchemy-works.github.io/magic/lu2_pack.js'
 
+export const { css, cx } = window.emotion
 export const html = window.html
 export const { render, h } = window.preact
