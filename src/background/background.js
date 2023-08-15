484297bc6fdb0fa65fd61957efb6f0d8c0f8252c
cbdyzj <cbdyzj@jianzhao.org>
2023-08-15 22:55:44 +0800

    Refactor code style

--- src/background/background.js
+++ src/background/background.js
@@ -7,41 +7,49 @@ function injectScript({ url }) {
     document.head.appendChild(scriptRef)
 }
 
-async function main() {
-
-    chrome.action.onClicked.addListener(async () => {
-        await chrome.runtime.openOptionsPage()
+async function onInjectScript({ tabId, url }) {
+    const state = await getAppState()
+    const config = state.configList.find((it) => {
+        return new RegExp(it.pattern).test(url);
     })
-
-
-    chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-        const state = await getAppState()
-        const config = state.configList.find((it) => {
-            return new RegExp(it.pattern).test(sender.url);
-        })
-        if (!config) {
-            return
-        }
-        await chrome.scripting.executeScript({
-            target: {
-                tabId: sender.tab.id,
-            },
-            func: injectScript,
-            args: [config],
-            world: 'MAIN',
-        })
+    if (!config) {
+        return
+    }
+    await chrome.scripting.executeScript({
+        target: {
+            tabId,
+        },
+        func: injectScript,
+        args: [config],
+        world: 'MAIN',
     })
+}
 
-    chrome.scripting
-        .registerContentScripts([{
+async function applyRegisterContentScripts() {
+    try {
+        await chrome.scripting.registerContentScripts([{
             id: 'user-script',
             js: ['user_script.js'],
             persistAcrossSessions: false,
             matches: ['*://*/*'],
             runAt: 'document_idle',
         }])
-        .then(() => console.log('registration complete'))
-        .catch((err) => console.warn('unexpected error', err))
+        console.log('registration complete')
+    } catch (err) {
+        console.warn('unexpected error', err)
+    }
+}
+
+async function main() {
+    chrome.action.onClicked.addListener(async () => {
+        await chrome.runtime.openOptionsPage()
+    })
+
+    chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+        await onInjectScript({ tabId: sender.tab.id, url: sender.url, })
+    })
+
+    await applyRegisterContentScripts()
 }
 
 main().catch((err) => console.error(err))
